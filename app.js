39162bbfc00ab99e4780a8d8570c17382d9647bc
bruceabrigo <bruceabrigo@outlook.com>
2023-03-22 11:16:09 -0500

    Users can now take in input and select pre-defined tip options

--- app.js
+++ app.js
@@ -1,31 +1,82 @@
+const tipTotal = document.getElementById('tip-total')
+const perPerson = document.getElementById('person-total')
+
 const fivePercent = () => {
     let amnt = document.getElementById('checkAmount').value
-    tip = amnt * 0.05
-    console.log(tip)
+    let ppl = document.getElementById('numberPpl').value
+
+    if (amnt == 0) {
+        console.log("Can't be zero")
+    } else {
+        tip = amnt * 0.05
+        console.log(tip)
+        tipTotal.innerText = tip
+        perPerson.innerText = tip/ppl
+    }
 }
 const tenPercent = () => {
     let amnt = document.getElementById('checkAmount').value
-    tip = amnt * 0.10
-    console.log(tip)
+    let ppl = document.getElementById('numberPpl').value
+
+    if (amnt == 0) {
+        console.log("can't be zero")
+    } else {
+        tip = amnt * 0.10
+        console.log(tip)
+        tipTotal.innerText = tip
+        perPerson.innerText = tip/ppl
+    }
 }
 const fifteenPercent = () => {
     let amnt = document.getElementById('checkAmount').value
-    tip = amnt * 0.15
-    console.log(tip)
+    let ppl = document.getElementById('numberPpl').value
+
+    if (amnt == 0) {
+        console.log('no zero')
+    } else {
+        tip = amnt * 0.15
+        console.log(tip)
+        tipTotal.innerText = tip
+        perPerson.innerText = tip/ppl
+    }
 }
 const twnetyFivePercent = () => {
     let amnt = document.getElementById('checkAmount').value
-    tip = amnt * 0.25
-    console.log(tip)
+    let ppl = document.getElementById('numberPpl').value
+
+    if (amnt == 0) {
+        console.log('no zero')
+    } else {
+        tip = amnt * 0.25
+        console.log(tip)
+        tipTotal.innerText = tip
+        perPerson.innerText = tip/ppl
+    }
 }
 const fiftyPercent = () => {
     let amnt = document.getElementById('checkAmount').value
-    tip = amnt * 0.50
-    console.log(tip)
+    let ppl = document.getElementById('numberPpl').value
+
+    if (amnt == 0) {
+        console.log('no zero')
+    } else {
+        tip = amnt * 0.50
+        console.log(tip)
+        tipTotal.innerText = tip
+        perPerson.innerText = tip/ppl
+    }
 }
 const customPercent = () => {
     let amnt = document.getElementById('checkAmount').value
     let customTip = document.getElementById('customTip').value
-    tip = amnt * customTip/100
-    console.log(tip)
+    let ppl = document.getElementById('numberPpl').value
+
+    if (amnt == 0) {
+        console.log('no zero')
+    } else {
+        tip = amnt * customTip/100
+        console.log(tip)
+        tipTotal.innerText = tip
+        perPerson.innerText = tip/ppl
+    }
 }
